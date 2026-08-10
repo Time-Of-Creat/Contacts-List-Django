100f84a0358f54b674a1e18cfbe2034661b99522
Time-Of-Creat <matkago12345new@gmail.com>
2026-08-10 20:00:45 +0300

    created tests for database

--- contacts/tests/test_database.py
+++ contacts/tests/test_database.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
+from ..models import Contact
 
 
 class ContactModelTest(TestCase):
@@ -14,5 +15,22 @@ class ContactModelTest(TestCase):
             'first_name': 'Иван',
             'last_name': 'Петров',
             "phone_number": "+70000000000",
-            "email": "test@test.tst"
+            "email": "test@test.tst",
+            'user': self.user
         }
+    
+    def testCreateUser(self):
+        contact = Contact.objects.create(**self.contact_data)
+
+        self.assertEqual(Contact.objects.count(), 1)
+
+        self.assertEqual(contact.first_name, 'Иван')
+        self.assertEqual(contact.last_name, 'Петров')
+        self.assertEqual(contact.phone_number, '+70000000000')
+        self.assertEqual(contact.email, "test@test.tst")
+        self.assertEqual(contact.user, self.user)
+
+    def teststrfyUser(self):
+        contact = Contact.objects.create(**self.contact_data)
+
+        self.assertEqual(str(contact), "Иван Петров - +70000000000")
